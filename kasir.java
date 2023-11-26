d109ed5a8a614f93d5a17c23dc0f089caad2542d
sayevvv <shamilbasayev10@gmail.com>
2023-11-26 13:30:22 +0700

    mebuat file tugas akhir berupa kasir :fire:

---
+++ kasir.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class kasir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //DEKLARASI BARANG BESERTA HARGANYA
+        int total;
+        int [] struk;
+        boolean lanjut = true;
+
+        //LIST BARANG
+        System.out.println("=========================");
+        System.out.println("       List Barang       ");
+        System.out.println("=========================");
+        System.out.println("1. Rokok:\tRp.5000");
+        System.out.println("2. Mie:\t\tRp.3500");
+        System.out.println("3. Minyak:\tRp.15000");
+
+        //INPUT BELANJA
+        
+    }
+}
